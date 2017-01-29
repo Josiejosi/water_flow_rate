7c492ede398984fd8171417553e6580f56b6dba4
vagrant <vagrant@homestead>
2017-01-29 08:57:06 +0000

    Front Layout Fix

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -17,6 +17,19 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+
+    <style type="text/css">
+
+
+        .login-box-body, .register-box-body {
+            padding-bottom: 20px;
+            height: 350px;
+        }
+
+    </style>
+
+
   </head>
   <body class="hold-transition login-page">
       <div class="login-box">
